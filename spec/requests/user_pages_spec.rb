7b32c492c775a51f78c10df4c3474a70f7b54d2c
Rory Everitt <everitt.rory@gmail.com>
2012-04-03 22:03:22 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,8 +50,17 @@ describe "User pages" do
       expect do
         click_button "Create my account"
       end.to change(User, :count).by(1)
-      end
     end
+      
+    describe "after saving the user" do
+      before { click_button "Create my account" }
+      let(:user) { User.find_by_email('user@example.com') }
+
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Sign out') }
+      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    end
+    
   end
-  
+end
 end
