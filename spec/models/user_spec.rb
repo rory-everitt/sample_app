5b193c1bfb0b6e5e16f588a7f67f7ff4e17b3d38
Rory Everitt <everitt.rory@gmail.com>
2012-04-06 18:57:25 -0700

    finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,16 +22,26 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:password_confirmation) }
+  
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }  
 
   it { should be_valid }
+  it { should_not be_admin }
   
+    describe "with admin attribute set to 'true'" do
+        before { @user.toggle!(:admin) }
+
+        it { should be_admin }
+    end
+    
     describe "remember token" do
       before { @user.save }
       its(:remember_token) { should_not be_blank }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,51 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        sign_in user
+        visit users_path
+      end
+
+      it { should have_selector('title', text: 'All users') }
+
+      describe "pagination" do
+        before(:all) { 30.times { FactoryGirl.create(:user) } }
+        after(:all)  { User.delete_all }
+
+        it { should have_link('Next') }
+        its(:html) { should match('>2</a>') }
+
+        it "should list each user" do
+          User.all[0..2].each do |user|
+            page.should have_selector('li', text: user.name)
+          end
+        end
+      end
+      
+      describe "delete links" do
+
+            it { should_not have_link('delete') }
+
+            describe "as an admin user" do
+              let(:admin) { FactoryGirl.create(:admin) }
+              before do
+                sign_in admin
+                visit users_path
+              end
+
+              it { should have_link('delete', href: user_path(User.first)) }
+              it "should be able to delete another user" do
+                expect { click_link('delete') }.to change(User, :count).by(-1)
+              end
+              it { should_not have_link('delete', href: user_path(admin)) }
+            end
+      end
+  end
+  
   describe "signup page" do
     before { visit signup_path }
 
@@ -23,44 +68,84 @@ describe "User pages" do
 
       before { visit signup_path }
 
-  describe "with invalid information" do
-    it "should not create a user" do
-      expect { click_button "Create my account" }.not_to change(User, :count)
-    end
+      describe "with invalid information" do
+        it "should not create a user" do
+          expect { click_button "Create my account" }.not_to change(User, :count)
+        end
     
-    describe "error messages" do
-           before { click_button "Create my account" }
+        describe "error messages" do
+               before { click_button "Create my account" }
 
-           it { should have_selector('title', text: 'Sign up') }
-           it { should have_content('error') }
-           it { should have_content('Email is invalid') }
-           it { should have_content('Password is too short') }
-    end
-  end
+               it { should have_selector('title', text: 'Sign up') }
+               it { should have_content('error') }
+               it { should have_content('Email is invalid') }
+               it { should have_content('Password is too short') }
+        end
+      end
 
-  describe "with valid information" do
-    before do
-      fill_in "Name",         with: "Example User"
-      fill_in "Email",        with: "user@example.com"
-      fill_in "Password",     with: "foobar"
-      fill_in "Confirmation", with: "foobar"
-    end
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
 
-    it "should create a user" do
-      expect do
-        click_button "Create my account"
-      end.to change(User, :count).by(1)
-    end
+        it "should create a user" do
+          expect do
+            click_button "Create my account"
+          end.to change(User, :count).by(1)
+        end
       
-    describe "after saving the user" do
-      before { click_button "Create my account" }
-      let(:user) { User.find_by_email('user@example.com') }
+        describe "after saving the user" do
+          before { click_button "Create my account" }
+          let(:user) { User.find_by_email('user@example.com') }
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Sign out') }
-      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-    end
-    
+          it { should have_selector('title', text: user.name) }
+          it { should have_link('Sign out') }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end   
+      end
   end
-end
-end
+  
+  describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+            sign_in user
+            visit edit_user_path(user)
+      end
+
+      describe "page" do
+        it { should have_selector('h1',    text: "Update your profile") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+
+        it { should have_content('error') }
+      end
+  
+      describe "with valid information" do
+            let(:new_name)  { "New Name" }
+            let(:new_email) { "new@example.com" }
+            before do
+              fill_in "Name",             with: new_name
+              fill_in "Email",            with: new_email
+              fill_in "Password",         with: user.password
+              fill_in "Confirm Password", with: user.password
+              click_button "Save changes"
+            end
+
+            it { should have_selector('title', text: new_name) }
+            it { should have_selector('div.alert.alert-success') }
+            it { should have_link('Sign out', :href => signout_path) }
+            specify { user.reload.name.should  == new_name }
+            specify { user.reload.email.should == new_email }
+      end
+
+  end
+  
+end  
+
